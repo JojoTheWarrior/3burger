a32f0ef90fb2c35f500870abedd31dc312c2fce7
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2025-09-23 18:17:05 -0400

    new url steak

--- frontend/index.js
+++ frontend/index.js
@@ -71,7 +71,7 @@ document.getElementById('orderForm').addEventListener('submit', async e => {
 
     console.log(orderData);
 
-    const URL = 'https://3fadc0b40739.ngrok-free.app/get_burger';
+    const URL = 'https://d1bc450aaa16.ngrok-free.app/get_burger';
 
     try {
         const response = await fetch(URL, {
