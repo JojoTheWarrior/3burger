e6e24120a95cc04412b7d1138253ac4fc3593c91
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2025-09-23 19:41:18 -0400

    luka server url

--- frontend/index.js
+++ frontend/index.js
@@ -71,7 +71,7 @@ document.getElementById('orderForm').addEventListener('submit', async e => {
 
     console.log(orderData);
 
-    const URL = 'https://1fe2b7394778.ngrok-free.app//get_burger';
+    const URL = 'https://b648df86ca9d.ngrok-free.app/get_burger';
 
     try {
         const response = await fetch(URL, {
