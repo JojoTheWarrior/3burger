3bfac5fbda0fb6df6d7dc0a2a190d1ba565e816e
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2025-09-25 23:01:34 -0400

    harveys url guitar

--- script.py
+++ script.py
@@ -81,7 +81,7 @@ def get_human_like_options():
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
     ]
-    options.add_argument(f"user-agent={random.choice(user_agents)}")
+    # options.add_argument(f"user-agent={random.choice(user_agents)}")
 
     # No automation flag in JS
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
