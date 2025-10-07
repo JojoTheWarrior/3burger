bf73663119d8f4d5e5cb25d186b748fd9435c38e
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2025-10-07 13:05:57 -0400

    changed url austin

--- frontend/index.js
+++ frontend/index.js
@@ -74,7 +74,7 @@ document.getElementById('orderForm').addEventListener('submit', async e => {
 
     // for local testing
     // const URL 
-    const URL = 'https://70df2719a6c9.ngrok-free.app/get_burger';
+    const URL = 'https://77da88f087e6.ngrok-free.app/get_burger';
 
     try {
         const response = await fetch(URL, {
