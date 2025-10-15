b6292b3148d072457fc59c77745f0635d3e4221b
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2025-10-15 16:37:50 -0400

    url richard

--- frontend/index.js
+++ frontend/index.js
@@ -74,7 +74,7 @@ document.getElementById('orderForm').addEventListener('submit', async e => {
 
     // for local testing
     // const URL 
-    const URL = 'https://19ace7df3775.ngrok-free.app/get_burger';
+    const URL = 'https://1a1286241145.ngrok-free.app/get_burger';
 
     try {
         const response = await fetch(URL, {
