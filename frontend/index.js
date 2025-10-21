e6370db296e69e29c6161a8f8650c0621936233c
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2025-10-21 17:23:57 -0400

    changed url kids

--- frontend/index.js
+++ frontend/index.js
@@ -74,7 +74,7 @@ document.getElementById('orderForm').addEventListener('submit', async e => {
 
     // for local testing
     // const URL 
-    const URL = 'https://1a1286241145.ngrok-free.app/get_burger';
+    const URL = 'https://134669e24de8.ngrok-free.app/get_burger';
 
     try {
         const response = await fetch(URL, {
