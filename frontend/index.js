986a3e21eac35b29fb190864fb9d064d8eadd973
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2025-12-08 17:00:19 -0500

    ngrok 5420 not 5000

--- frontend/index.js
+++ frontend/index.js
@@ -74,7 +74,7 @@ document.getElementById('orderForm').addEventListener('submit', async e => {
 
     // for local testing
     // const URL 
-    const URL = 'https://5f635b152539.ngrok-free.app/get_burger';
+    const URL = 'https://0974d9789546.ngrok-free.app/get_burger';
 
     try {
         const response = await fetch(URL, {
