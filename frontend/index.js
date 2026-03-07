039b8cfe785cb79db653f37113596109fde38ea1
JojoTheWarrior <joshua.wang828@gmail.com>
2026-03-06 22:13:28 -0500

    birger drunk

--- frontend/index.js
+++ frontend/index.js
@@ -74,7 +74,7 @@ document.getElementById('orderForm').addEventListener('submit', async e => {
 
     // for local testing
     // const URL 
-    const URL = 'https://e74be8f7866d.ngrok-free.app/get_burger';
+    const URL = 'https://13ce-129-97-125-16.ngrok-free.app/get_burger';
 
     try {
         const response = await fetch(URL, {
