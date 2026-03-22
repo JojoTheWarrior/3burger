c0c1b002dac6a4f4ebb50d2faa935c0bb986202c
JojoTheWarrior <joshua.wang828@gmail.com>
2026-03-21 23:41:25 -0400

    brian burger

--- frontend/index.js
+++ frontend/index.js
@@ -74,7 +74,7 @@ document.getElementById('orderForm').addEventListener('submit', async e => {
 
     // for local testing
     // const URL 
-    const URL = 'https://3b8a-129-97-125-131.ngrok-free.app/get_burger';
+    const URL = 'https://9386-129-97-125-131.ngrok-free.app/get_burger';
 
     try {
         const response = await fetch(URL, {
