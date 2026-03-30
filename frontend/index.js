c29ef5463ceca85a68d883c1643d639ab912fe86
JojoTheWarrior <joshua.wang828@gmail.com>
2026-03-30 19:47:56 -0400

    burger imc

--- frontend/index.js
+++ frontend/index.js
@@ -74,7 +74,7 @@ document.getElementById('orderForm').addEventListener('submit', async e => {
 
     // for local testing
     // const URL 
-    const URL = 'https://28e0-2620-101-f000-7c1-00-16ae.ngrok-free.app/get_burger';
+    const URL = 'https://5f93-2620-101-f000-7c1-00-b6ae.ngrok-free.app/get_burger';
 
     try {
         const response = await fetch(URL, {
